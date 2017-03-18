592f671ce3851cc6db056d9939297518044309d6
Annette Bowman <bowmanannette@gmail.com>
2017-03-18 19:46:14 -0400

    initial commit

---
+++ OddEvenCalculatorConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace OddEvenCalculatorConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            Console.WriteLine("Enter a number:");
+            number = Int32.Parse(Console.ReadLine());
+            OddEvenCalculatorConsoleApp.PlayWithNumbers playWithNumbers = new OddEvenCalculatorConsoleApp.PlayWithNumbers();
+            if (playWithNumbers.DetermineOddOrEven(number))
+            {
+                Console.WriteLine("Number is even");
+                return;
+            }
+            Console.WriteLine("Number is odd");
+        }
+    }
+
+    public class PlayWithNumbers
+    {
+        public bool DetermineOddOrEven(int number)
+        {
+            if (number % 2 == 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ Tests/Tests.cs
@@ -0,0 +1,26 @@
+﻿using OddEvenCalculatorConsoleApp;
+using Shouldly;
+using Xunit;
+
+namespace Tests
+{
+    public class Tests
+    {
+        PlayWithNumbers playWithNumbers = new PlayWithNumbers();
+        [Fact]
+        public void EvenTest()
+        {
+            int number = 8;
+            var result = playWithNumbers.DetermineOddOrEven(number);
+            result.ShouldBe(true);
+        }
+        [Fact]
+        public void OddTest()
+        {
+            int number = 9;
+            var result = playWithNumbers.DetermineOddOrEven(number);
+            result.ShouldBe(false);
+        }
+    }
+}
+
